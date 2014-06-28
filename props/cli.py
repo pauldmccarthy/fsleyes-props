41242b47d1e52c880c331d59fb232ad8057e142d
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-06-28 14:03:50 +0100

    cli.py documentation

--- props/cli.py
+++ props/cli.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 #
-# cli.py - Generate command line arguments for a HasPropertyies object.
-#
-# This module provides two functions:
-#  - addParserArguments: Given an argparser.ArgumentParser and a
-#                        HasProperties class (or instance), adds arguments
-#                        to the parser for each PropertyBase attribute of
-#                        the HasProperties class.
-#
-#  - applyArguments:     Given a HasProperties instance and an
-#                        argparse.Namespace object assumed to have been
-#                        created by the parser mentioned above, sets the
-#                        property values of the HasProperties instance from
-#                        the values stored in the Namespace object.
+# cli.py - Generate command line arguments for a HasProperties object.
 #
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
+
+"""Generate command line arguments for a
+:class:`~props.properties.HasProperties object.
+
+This module provides two functions:
+
+ - :func:`addParserArguments`: Given an :class:`argparse.ArgumentParser` and a
+                               :class:`~fsl.props.properties.`HasProperties`
+                               class (or instance), adds arguments
+                               to the parser for each
+                               :class:`~fsl.props.properties.PropertyBase
+                               attribute of the
+                               :class:`~fsl.props.properties.`HasProperties`
+                               class.
+
+ - :func:`applyArguments`:     Given a
+                               :class:`~fsl.props.properties.`HasProperties`
+                               instance and an :class:`argparse.Namespace`
+                               object assumed to have been created by the
+                               parser mentioned above, sets the property
+                               values of the
+                               :class:`~fsl.props.properties.`HasProperties`
+                               instance from the values stored in the
+                               :class:`~argparse.Namespace` object.
+"""
 
 import logging
 log = logging.getLogger(__name__)
@@ -27,31 +40,33 @@ import matplotlib.cm as mplcm
 
 import properties as props
 
-# The functions below add an argument to an ArgumentParser for a
-# specific property type.
+# The functions below add an argument to an 
+# ArgumentParser for a specific property type.
 
 def _String(parser, propCls, propName, propHelp, shortArg, longArg):
-    """
-    Adds an argument to the given parser for the given String property.
+    """Adds an argument to the given parser for the given
+    :class:`~props.properties_types.String` property.
     
-    Parameters:
+    :param parser:       An :class:`argparse.ArgumentParser` instance.
     
-      - parser:   An argparse.ArgumentParser instance.
+    :param propCls:      A :class:`fsl.props.properties.HasProperties` class.
     
-      - propCls:  A HasProperties class.
+    :param str propName: Name of the property.
     
-      - propName: Name of the property.
+    :param str propHelp: Custom help text for the property.
     
-      - propHelp: Custom help text for the property.
+    :param str shortArg: String to use as the short argument.
     
-      - shortArg: String to use as the short argument.
-    
-      - longArg:  String to use as the long argument.
+    :param str longArg:  String to use as the long argument.
     """
     parser.add_argument(shortArg, longArg, help=propHelp) 
 
     
 def _Boolean(parser, propCls, propName, propHelp, shortArg, longArg):
+    """Adds an argument to the given parser for the given
+    :class:`~props.properties_types.Boolean` property. See the
+    :func:`_String` documentation for details on the parameters.
+    """
     parser.add_argument(shortArg,
                         longArg,
                         help=propHelp,
@@ -59,6 +74,10 @@ def _Boolean(parser, propCls, propName, propHelp, shortArg, longArg):
 
     
 def _Int(parser, propCls, propName, propHelp, shortArg, longArg):
+    """Adds an argument to the given parser for the given
+    :class:`~props.properties_types.Int` property. See the
+    :func:`_String` documentation for details on the parameters.
+    """ 
     parser.add_argument(shortArg,
                         longArg,
                         help=propHelp,
@@ -67,6 +86,10 @@ def _Int(parser, propCls, propName, propHelp, shortArg, longArg):
 
     
 def _Real(parser, propCls, propName, propHelp, shortArg, longArg):
+    """Adds an argument to the given parser for the given
+    :class:`~props.properties_types.Real` property. See the
+    :func:`_String` documentation for details on the parameters.
+    """ 
     parser.add_argument(shortArg,
                         longArg,
                         help=propHelp,
@@ -75,6 +98,10 @@ def _Real(parser, propCls, propName, propHelp, shortArg, longArg):
 
 
 def _Bounds(parser, propCls, propName, propHelp, shortArg, longArg):
+    """Adds an argument to the given parser for the given
+    :class:`~props.properties_types.Bounds` property. See the
+    :func:`_String` documentation for details on the parameters.
+    """ 
     ndims = getattr(propCls, propName)._ndims
     parser.add_argument(shortArg,
                         longArg,
@@ -85,7 +112,10 @@ def _Bounds(parser, propCls, propName, propHelp, shortArg, longArg):
 
     
 def _ColourMap(parser, propCls, propName, propHelp, shortArg, longArg):
-
+    """Adds an argument to the given parser for the given
+    :class:`~props.properties_types.ColourMap` property. See the
+    :func:`_String` documentation for details on the parameters.
+    """ 
     # Attempt to retrieve a matplotlib.cm.ColourMap
     # instance given its name
     def parse(cmapName):
@@ -105,10 +135,18 @@ def _ColourMap(parser, propCls, propName, propHelp, shortArg, longArg):
 
 
 def applyArguments(hasProps, arguments):
-    """
-    Given a HasProperties instance and an argparser.Namespace instance,
-    sets the property values of the HasProperties instance from the
-    values stored in the Namespace object.
+    """Apply arguments to a :class:`~fsl.props.properties.HasProperties`
+    instance.
+
+    Given a :class:`~fsl.props.properties.HasProperties` instance and an
+    :class:`argparse.Namespace` instance, sets the property values of the
+    :class:`~fsl.props.properties.HasProperties` instance from the values
+    stored in the :class:`argparse.Namespace` object.
+
+    :param hasProps:  The :class:`~fsl.props.properties.HasProperties`
+                      instance.
+    
+    :param arguments: The :class:`argparse.Namespace` instance.
     """
     
     propNames, propObjs = hasProps.getAllProperties()
@@ -122,11 +160,17 @@ def applyArguments(hasProps, arguments):
 
     
 def _getShortArgs(propCls, propNames, exclude=''):
-    """
-    Generates unique single-letter argument names for each
-    of the names in the given list of property names. Any
-    letters in the exclude string are not used as short
-    arguments.
+    """Generates unique single-letter argument names for each of the names in
+    the given list of property names. Any letters in the exclude string are
+    not used as short arguments.
+
+    :param propCls:        A :class:`~fsl.props.properties.HasProperties`
+                           class.
+    
+    :param list propNames: List of property names for which short arguments 
+                           are to be generated.
+    
+    :param str exclude:    String containing letters which should not be used.
     """
 
     letters   = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -181,42 +225,45 @@ def addParserArguments(
         longArgs=None,
         propHelp=None,
         exclude=''):
-    """
-    Adds arguments to the given argparse.ArgumentParser, providing a command
-    line interface to the properties of the given HasProperties object.
-    Parameters:
+    """Adds arguments to the given :class:`argparse.ArgumentParser` for the
+    properties of the given :class:`~props.properties.HasProperties` object.
 
-      - propCls:   A props.HasProperties class. An instance may alternately
-                   be passed.
+    :param propCls:        A :class:`~props.properties.HasProperties` class. 
+                           An instance may alternately be passed in.
     
-      - parser:    An argparse.ArgumentParser to add arguments to.
+    :param parser:         An :class:`argparse.ArgumentParser` to add
+                           arguments to.
     
-      - cliProps:  List containing the names of properties to add arguments
-                   for. If None, and an attribute called '_cliProps' is
-                   present on the hasProps object, the value of that attribute
-                   is used. Otherwise an argument is added for all properties.
+    :param list cliProps:  List containing the names of properties to add
+                           arguments for. If ``None``, and an attribute called
+                           ``_cliProps``' is present on the ``propCls`` class,
+                           the value of that attribute is used. Otherwise an
+                           argument is added for all properties.
     
-      - shortArgs: Dict containing {propName: shortArg} mappings, to be used
-                   as the short (typically single letter) argument flag for
-                   each property. If None, and an attribute called '_shortArgs'
-                   is present on the hasProps object, the value of that
-                   attribute is used. Otherwise, short arguments are
-                   automatically generated for each property.
+    :param dict shortArgs: Dict containing ``{propName: shortArg}`` mappings,
+                           to be used as the short (typically single letter)
+                           argument flag for each property. If ``None``, and 
+                           an attribute called ``_shortArgs`` is present on
+                           the ``propCls`` class, the value of that attribute
+                           is used. Otherwise, short arguments are
+                           automatically generated for each property.
     
-      - longArgs:  Dict containing {propName: longArg} mappings, to be used
-                   as the long argument flag for each property. If None, and
-                   an attribute called '_longArgs' is present on the hasProps
-                   object, the value of that attribute is used. Otherwise,
-                   the name of each property is used as its long argument.
+    :param dict longArgs:  Dict containing ``{propName: longArg}`` mappings,
+                           to be used as the long argument flag for each
+                           property. If ``None``, and an attribute called
+                           ``_longArgs`` is present on the ``propCls`` class,
+                           the value of that attribute is used. Otherwise, the
+                           name of each property is used as its long argument.
     
-      - propHelp:  Dict containing {propName: helpString] mappings, to be used
-                   as the help text for each property. If None, and an
-                   attribute called '_propHelp' is present on the hasProps
-                   object, the value of that attribute is used. Otherwise,
-                   no help string is used.
+    :param dict propHelp:  Dict containing ``{propName: helpString]``
+                           mappings, to be used as the help text for each
+                           property. If ``None``, and an attribute called
+                           ``_propHelp`` is present on the ``propCls`` class,
+                           the value of that attribute is used. Otherwise, no
+                           help string is used.
     
-      - exclude:   String containing letters which should not be used as short
-                   arguments.
+    :param str exclude:    String containing letters which should not be used
+                           as short arguments.
     """
 
     if isinstance(propCls, props.HasProperties):
