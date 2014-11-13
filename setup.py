3ecc7c9551b80820639ec8434ff5e99b839770a9
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-11-13 11:27:16 +0000

    Making props package installable via pip

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+# setup.py - setuptools configuration for installing the props package.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+
+    name='props',
+
+    version='0.0',
+
+    description='Python descriptor framework',
+
+    url='https://git.fmrib.ox.ac.uk/paulmc/props',
+
+
+    author='Paul McCarthy',
+
+    author_email='pauldmccarthy@gmail.com',
+
+    license='FMRIB',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Free for non-commercial use',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+
+    packages=find_packages(exclude=('doc'))
+)
