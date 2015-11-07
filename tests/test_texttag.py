a13a1342aab9be4d40f0c368b9a6ed754e2cc99f
Paul McCarthy <pauld.mccarthy@gmail.com>
2015-11-07 16:42:18 +0000

    I have had to roll my own auto-completing text contol, due to the incompetence of the wxWidgets framework. Needs a bit more tweaking.

--- tests/test_texttag.py
+++ tests/test_texttag.py
@@ -5,24 +5,69 @@
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
 
+import logging
+logging.basicConfig(
+    format='%(levelname)8.8s '
+           '%(filename)20.20s '
+           '%(lineno)4d: '
+           '%(funcName)-15.15s - '
+           '%(message)s')
+
+logging.getLogger('pwidgets').setLevel(logging.DEBUG)
+
 import wx
 
-import pwidgets.texttag as texttag
+import pwidgets.texttag      as texttag
+import pwidgets.autotextctrl as autotextctrl
 
 
-app      = wx.App()
-frame    = wx.Frame(None)
-sizer    = wx.BoxSizer(wx.HORIZONTAL)
-tagpanel = texttag.TextTagPanel(frame,
-                                style=(texttag.TTP_ALLOW_NEW_TAGS |
-                                       texttag.TTP_ADD_NEW_TAGS   |
-                                       texttag.TTP_NO_DUPLICATES))
+options = ['signal',
+           'sign',
+           'sausage',
+           'sasquatch',
+           'seepage',
+           'silly',
+           'sumptuous',
+           'noise',
+           'nose',
+           'nobby',
+           'noddy',
+           'nibble',
+           'no',
+           'not',
+           'never'
+           'grey matter',
+           'green',
+           'gooey',
+           'gabbo',
+           'gummy',
+           'great']
 
-tagpanel.SetOptions(('signal', 'noise', 'grey matter'))
+app       = wx.App()
+frame     = wx.Frame(None)
+sizer     = wx.BoxSizer(wx.VERTICAL)
 
-tagpanel.SetBackgroundColour((255, 255, 255))
+atc       = autotextctrl.AutoTextCtrl(frame)
 
-sizer.Add(tagpanel, flag=wx.EXPAND, proportion=1)
+tagpanel1 = texttag.TextTagPanel(frame,
+                                 style=(texttag.TTP_ALLOW_NEW_TAGS |
+                                        texttag.TTP_ADD_NEW_TAGS   |
+                                        texttag.TTP_NO_DUPLICATES))
+tagpanel2 = texttag.TextTagPanel(frame,
+                                 style=(texttag.TTP_ALLOW_NEW_TAGS |
+                                        texttag.TTP_NO_DUPLICATES))
+tagpanel3 = texttag.TextTagPanel(frame, style=texttag.TTP_NO_DUPLICATES)
+
+tagpanel1.SetOptions(  options)
+tagpanel2.SetOptions(  options)
+tagpanel3.SetOptions(  options)
+atc      .AutoComplete(options)
+
+
+sizer.Add(tagpanel1, flag=wx.EXPAND)
+sizer.Add(tagpanel2, flag=wx.EXPAND)
+sizer.Add(tagpanel3, flag=wx.EXPAND)
+sizer.Add(atc,       flag=wx.EXPAND)
 
 frame.SetSizer(sizer)
 frame.SetSize((600, 400))
