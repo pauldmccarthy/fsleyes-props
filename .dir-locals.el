583c9b511ba0a9ade3752a33c830ca7309b14c1b
Paul McCarthy <pauld.mccarthy@gmail.com>
2017-04-20 18:06:28 +0100

    Should never have been in git

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((python-mode . ((project-venv-name . "fslpy")))
-
- ; make our bodgy wx widgets installation work nicely with emacs
- (nil . ((eval . (setenv "PYTHONHOME" "/Users/paulmc/.virtualenvs/fslpy")))))
