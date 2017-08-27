01c395a13a3f1bb324ba94c57433e9010c406bd7
Paul McCarthy <pauldmccarthy@gmail.com>
2017-08-27 12:51:40 -0700

    Some basic unit tests. Need more of these.

--- tests/test_properties_value.py
+++ tests/test_properties_value.py
@@ -30,13 +30,49 @@ def test_listener():
     def l2(*a):
         called['l2'] = called.get('l2', 0) + 1
 
-    pv.addListener('l1', l1)
-    pv.addListener('l2', l2)
+    def al1(*a):
+        called['al1'] = called.get('al1', 0) + 1
+
+    def al2(*a):
+        called['al2'] = called.get('al2', 0) + 1
+
+    pv.addListener(         'l1',  l1)
+    pv.addListener(         'l2',  l2)
+    pv.addAttributeListener('al1', al1)
+    pv.addAttributeListener('al2', al2)
 
     assert pv.hasListener('l1')
     assert pv.hasListener('l2')
 
     pv.set('New value')
 
-    assert called['l1'] == 1
-    assert called['l2'] == 1
+    assert called['l1']  == 1
+    assert called['l2']  == 1
+
+    pv.set('New value')
+
+    assert called['l1']  == 1
+    assert called['l2']  == 1
+
+    pv.set('New new value')
+
+    assert called['l1']  == 2
+    assert called['l2']  == 2
+
+    assert called.get('al1') is None
+    assert called.get('al2') is None
+
+    pv.setAttribute('newAtt', 'value')
+
+    assert called['al1'] == 1
+    assert called['al2'] == 1
+
+    pv.setAttribute('newAtt', 'value')
+
+    assert called['al1'] == 1
+    assert called['al2'] == 1
+
+    pv.setAttribute('newAtt', 'new value')
+
+    assert called['al1'] == 2
+    assert called['al2'] == 2
