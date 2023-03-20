cf433aa2ffedfc70aac34c8ed54ea6a30623be51
Paul McCarthy <pauldmccarthy@gmail.com>
2023-03-20 10:48:14 +0000

    TEST: test self reference

---
+++ tests/test_hasprops.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# test_hasprops.py -
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+
+
+import fsleyes_props as props
+
+
+def test_self_reference():
+
+    class Thing(props.HasProps):
+        ref = props.Object()
+
+    t = Thing()
+
+    vals = [None, 123, 'abc', t]
+
+    for v in vals:
+        t.ref = v
+        print(t)
